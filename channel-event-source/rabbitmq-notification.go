26e29268da23627227c185ad6b916143e787119d
Justin Brulotte <justin.brlotte797@gmail.com>
2019-02-21 16:07:16 -0500

    Adding Channel implementation

---
+++ channel-event-source/rabbitmq-notification.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"log"
+	"net/http"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func sendNotification(sink string) {
+	message := map[string]interface{}{
+		"hello": "world",
+		"sink":  sink,
+	}
+
+	bytesRepresentation, err := json.Marshal(message)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post(sink, "application/json", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var result map[string]interface{}
+
+	json.NewDecoder(resp.Body).Decode(&result)
+
+	log.Printf(" [.] Responce %s", result)
+}
+
+func main() {
+	var sink string
+	flag.StringVar(&sink, "sink", "", "logger.default") // TODO  have a good default value
+	flag.Parse()
+
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"knative", // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		false,  // auto ack
+		false,  // exclusive
+		false,  // no local
+		false,  // no wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range msgs {
+			// Every things that come here is for hello-world-knative
+			sink := "http://" + sink + ".svc.cluster.local/"
+			log.Printf(" [x] %s, %s", d.Body, sink)
+			sendNotification(sink)
+			d.Ack(false)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+	<-forever
+}
