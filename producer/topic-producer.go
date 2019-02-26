43ce106490e008438a3c57ed242ff11aeb44fb48
Justin Brulotte <justin.brlotte797@gmail.com>
2019-02-26 11:44:30 -0500

    Edit Producer

--- producer/topic-producer.go
+++ producer/topic-producer.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"strconv"
 	"time"
 
 	"github.com/streadway/amqp"
@@ -14,32 +16,40 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
+	var routingKey string
+	flag.StringVar(&routingKey, "routingKey", "", "") // If there is no routing Key there will be a error
+	flag.Parse()
+	if routingKey == "" {
+		log.Fatalf("Flag Routing Key have to been pass")
+	}
+
+	// Connect to the RabbitMQ broker in the cluster
 	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
-
 	ch, err := conn.Channel()
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"knative", // name
-		false,     // durable
-		false,     // delete when unused
-		false,     // exclusive
-		false,     // no-wait
-		nil,       // arguments
+	// Connect to the Exchange
+	err = ch.ExchangeDeclare(
+		"knative-exchange", // name
+		"topic",            // type
+		true,               // durable
+		false,              // auto-deleted
+		false,              // internal
+		false,              // no-wait
+		nil,                // arguments
 	)
-	failOnError(err, "Failed to declare a queue")
-
-	body := "logger"
+	failOnError(err, "Failed to declare an exchange")
 
 	for i := 0; i < 5; i++ {
+		body := strconv.Itoa(i)
 		err = ch.Publish(
-			"",     // exchange
-			q.Name, // routing key
-			false,  // mandatory
-			false,  // immediate
+			"knative-exchange", // exchange
+			routingKey,         // routing key
+			false,              // mandatory
+			false,              // immediate
 			amqp.Publishing{
 				ContentType: "text/plain",
 				Body:        []byte(body),
@@ -47,7 +57,6 @@ func main() {
 		failOnError(err, "Failed to publish a message")
 
 		log.Printf(" [x] Sent %s", body)
-		body = "logger2"
 		time.Sleep(1 * time.Second)
 	}
 }
